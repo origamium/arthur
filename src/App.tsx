67f12194a5b141e6060bdd06451620574e2ed7e3
ONOUE Origami <arclisp@plotdot.net>
2025-07-26 00:21:07 +0900

    はい

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,15 @@
-import { useState } from "react"
-import viteLogo from "/vite.svg"
-import reactLogo from "./assets/react.svg"
-import "./App.css"
+import { css } from "@emotion/react"
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noopener">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noopener">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button type="button" onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="grid place-items-center">
+      <h1
+        css={css`
+          background-color: #f0f0f0;
+      `}
+      >
+        Hello!
+      </h1>
+    </div>
   )
 }
-
-export default App
